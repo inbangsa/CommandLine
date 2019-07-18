936930534c0a28ba8119da80df12c31903dbffc2
tamerharsh <harsh.tamrakar@ifm.com>
2019-07-18 16:08:40 +0530

    [#3] made changes according to comments.

--- commandlineparser/src/Parser.cpp
+++ commandlineparser/src/Parser.cpp
@@ -1,20 +1,31 @@
 #include "Parser.h"
 
-void cmdParser::Parser::add_options(std::string short_command ,std::string long_command,std::string short_description, std::string long_description)
-{	
-	opts::Options *obj=new opts::Options(short_command,long_command,short_description,long_description);
+void cmdParser::Parser::add_options(std::string short_command, std::string long_command, std::string short_description, std::string long_description) 
+{
+	std::shared_ptr<cmdParser::Options> obj = std::make_shared<cmdParser::Options>(short_command, long_command, short_description, long_description);
 	
-	if(!(obj->get_option_short_command().empty()))
-	{	
-		command_list.insert(std::pair<std::string,opts::Options*>(obj->get_option_short_command(),obj));
-	}	
-	else if(!(obj->get_option_long_command().empty()))
+	//if both  short and command a present make both  of them individually as key.
+
+	if ((!(obj->get_option_short_command().empty())) && (!(obj->get_option_long_command().empty())))
 	{
-		command_list.insert(std::pair<std::string,opts::Options*>(obj->get_option_long_command(),obj));
-		
+		command_list.insert(KeyValue(obj->get_option_short_command(), obj));
+		command_list.insert(KeyValue(obj->get_option_long_command(), obj));
+	}
+	//if only  short command  is present ,make short command as key.
+	else if (!(obj->get_option_short_command().empty()))
+	{
+		command_list.insert(KeyValue(obj->get_option_short_command(), obj));		
+	}
+	//if only long command is present, make long command as key.
+
+	else if (!(obj->get_option_long_command().empty()))
+	{
+		command_list.insert(KeyValue(obj->get_option_long_command(), obj));
 	}
 	else
 	{
-		std::cout<<"Enter a valid short or long command or both";
+		throw std::runtime_error("Enter a valid short or long command or both.");
 	}
 }
+
+
--- sample/sample_user.cpp
+++ sample/sample_user.cpp
@@ -1,9 +1,18 @@
-#include<iostream>
-int main()
+#include "Parser.h"
+int  main(int argc, char*argv[])
 {
+	cmdParser::Parser obj;
+	try
+	{
+		obj.add_options("", "port", "short description ", "very very long description");
+		obj.add_options("cp", "copy", "short description ", "very very long description");
+		obj.add_options("IP", "", "short description ", "very very long description");
+		obj.add_options("", "", "short description ", "very very long description");
+	}
+	catch (std::exception &e)
+	{
+		std::cout << "[ Error ]:" << e.what();
+	}
 	
-	
-	
-	
-	std::cin.get();
+return 0;
 }
