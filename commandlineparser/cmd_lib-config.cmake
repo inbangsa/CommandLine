418d821f0038064338e80e07d896f2e263105bbd
“your <harsh.tamrakar@ifm.com>
2019-09-03 17:46:03 +0530

    [#20] implemented the find package for userdefined build type.

--- commandlineparser/cmd_lib-config.cmake
+++ commandlineparser/cmd_lib-config.cmake
@@ -1,8 +1,2 @@
-#get_filename_component(SELF_DIR ${CMAKE_CURRENT_LIST_FILE} PATH)
-#message("**************"${SELF_DIR})
-#include(${SELF_DIR}/cmd_lib.cmake)
- 
- get_filename_component(cmd_lib_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
- #message("**************"${cmd_lib_DIR})
-set(cms_lib_INCLUDE_DIRS include)
-#message(STATUS "RapidJSON found. Headers: ${RAPIDJSON_INCLUDE_DIRS}")
+get_filename_component(SELF_DIR ${CMAKE_CURRENT_LIST_FILE} PATH)
+include(${SELF_DIR}/lib/${CMAKE_BUILD_TYPE}/cmd_lib.cmake)
